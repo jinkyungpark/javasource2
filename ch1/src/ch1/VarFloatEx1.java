e51af2764172c5c7b9ca0ae54e3c1e1a22d52d04
kyung <kyung5896@gmail.com>
2024-08-20 18:16:38 +0900

    변수선언,초기화 개념

---
+++ ch1/src/ch1/VarFloatEx1.java
@@ -0,0 +1,25 @@
+package ch1;
+
+public class VarFloatEx1 {
+    public static void main(String[] args) {
+        // 타입 변수명 = 초기화값;
+        // F or f 가능(실수는 double로 기본타입으로 인식하기 때문에 float를 인지시켜줘야 함)
+        float score = 90.14f;
+
+        // D or d 가능(생략가능)
+        double score2 = 98.5d;
+        score2 = 90d;
+
+        System.out.println("score = " + score);
+        System.out.println("score2 = " + score2); // 90.0
+
+        int value1 = 92;
+        // 큰 타입 = 작은타입(O)
+        // double(8 byte) > int(4 byte)
+        double score3 = value1;
+        System.out.println("score3 = " + score3);
+
+        // 작은 타입 = 큰 타입(X) ==> 처리를 할 수는 있으나 값의 손실이 일어날 수 있다
+        // int value2 = score2;
+    }
+}
