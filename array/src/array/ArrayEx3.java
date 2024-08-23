5218b00dc011ddad20325cb1a66c0a8b728cafdb
kyung <kyung5896@gmail.com>
2024-08-23 17:19:27 +0900

    배열, 클래스

--- array/src/array/ArrayEx3.java
+++ array/src/array/ArrayEx3.java
@@ -1,5 +1,7 @@
 package array;
 
+import java.util.Arrays;
+
 public class ArrayEx3 {
     public static void main(String[] args) {
         // char 배열 선언, 생성
@@ -8,12 +10,34 @@ public class ArrayEx3 {
 
         char[] chArr = { 'a', 'b', 'c', 'd' };
 
+        System.out.println(Arrays.toString(chArr)); // [a, b, c, d]
+
+        for (int i = 0; i < chArr.length; i++) {
+            System.out.print(chArr[i] + "\t");
+        }
+
+        System.out.println();
+
+        // 향상된 for문
+        for (char c : chArr) {
+            System.out.print(c + "\t");
+        }
+
         // float 배열 선언, 생성
         float[] flArr = { 12.5f, 13.6f, 14.5f, 13.7f };
 
+        for (float f : flArr) {
+            System.out.println(f);
+        }
+
         // String 배열 선언, 생성
         String[] strArr = { "A", "B", "C", "D" };
+
+        for (String string : strArr) {
+            System.out.println(string);
+        }
+
         // ArrayIndexOutOfBoundsException
-        System.out.println(strArr[4]);
+        // System.out.println(strArr[4]);
     }
 }
--- array/src/array/ArrayEx4.java
+++ array/src/array/ArrayEx4.java
@@ -32,8 +32,8 @@ public class ArrayEx4 {
             jumsu2[i] = sc.nextInt();
         }
 
-        // 확인
-        System.out.println(Arrays.toString(jumsu2));
+        // 확인(화면 출력)
+        System.out.println(Arrays.toString(jumsu2)); // [88, 78, 69, 55, 45, 66, 78, 72, 53, 22, 55, 65, 75, 85, 92]
 
         sc.close();
 
